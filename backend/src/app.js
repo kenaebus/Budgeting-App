2f8fa2a5afde2c144a1b930ece8538cf0b912577
Kena <kenaebustos@gmail.com>
2024-07-10 13:32:48 -0400

    Install fonts styling

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,19 +1,25 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
 const cors = require('cors');
 const path = require('path');
-// const connectDB = require('./config/db');
+const connectDB = require('./config/db');
 
 dotenv.config();
+connectDB();
 
 const app = express();
 
+app.use(cors());
+app.use(bodyParser.json());
+
+
+// Connect to REACT App 
 app.use(express.static(path.join(__dirname, '../build')));
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../build', 'index.html'));
 });
-
 
 
 const PORT = process.env.PORT || 5000;
